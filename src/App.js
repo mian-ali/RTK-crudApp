5369521768d02495bae8faa6bce02524e8208fdd
Ali Ahmad <mian41935@gmail.com>
2022-10-30 01:11:59 +0500

    create & config store into app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
+import { useGetAllPostQuery } from './services/postingApi';
+
 function App() {
+  const res = useGetAllPostQuery();
+  console.log(res);
   return (
     <div className="App">
       <h1>Hello</h1>
