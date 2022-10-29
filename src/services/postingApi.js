2884452c8cc3f942748384f405f18b9e3ff84336
Ali Ahmad <mian41935@gmail.com>
2022-10-30 00:37:16 +0500

    RTK Basic Crud App

---
+++ src/services/postingApi.js
@@ -0,0 +1,21 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const postingApi = createApi({
+  reducerPath: 'postingApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
+  }),
+
+  //end ponit
+  endpoints: (builder) => ({
+    getAllPost: builder.query({
+      query: () => ({
+        url: 'posts',
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllPostQuery } = postingApi;
